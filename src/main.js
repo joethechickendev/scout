6c210eaa57319be71ddfee32a7f3da3911c4bab4
joethechickendev <wc28061@student.fccps.org>
2026-02-01 14:12:49 -0500

    make it look cool

--- src/main.js
+++ src/main.js
@@ -1,3 +1,137 @@
+const compList = [
+  {
+    name: "2026mdpas",
+    teams: [
+      "10679",
+      "1111",
+      "11350",
+      "11489",
+      "1418",
+      "1629",
+      "1719",
+      "2199",
+      "2421",
+      "2537",
+      "2849",
+      "2900",
+      "3748",
+      "4464",
+      "4505",
+      "4541",
+      "4821",
+      "5115",
+      "5830",
+      "5841",
+      "614",
+      "623",
+      "6239",
+      "6326",
+      "686",
+      "6863",
+      "7770",
+      "7886",
+      "836",
+      "8590",
+      "8622",
+      "8726",
+      "888",
+      "9033",
+      "9072"
+    ]
+  },
+  {
+    name: "2026mdsev",
+    teams: [
+      "10449",
+      "10679",
+      "1111",
+      "11211",
+      "11318",
+      "11350",
+      "1389",
+      "1418",
+      "1446",
+      "1629",
+      "1719",
+      "1727",
+      "1915",
+      "2199",
+      "2377",
+      "2534",
+      "2849",
+      "2912",
+      "2914",
+      "2963",
+      "339",
+      "3714",
+      "3748",
+      "4505",
+      "4541",
+      "614",
+      "623",
+      "686",
+      "6863",
+      "7770",
+      "836",
+      "8592",
+      "8622",
+      "8726",
+      "9033"
+    ]
+  }
+]
+
+function createOption(v,str) {
+  let option = document.createElement('option');
+  option.value = v;
+  option.textContent = str;
+  
+  return option;
+}
+
+function updateCompList() {
+  const comp = document.getElementById("comp");
+  comp.innerHTML = "";
+
+  comp.appendChild(createOption("","N/A"));
+  for(const c of compList) {
+    comp.appendChild(createOption(c.name,c.name));
+  }
+}
+
+function updateTeamList() {
+  const team = document.getElementById("team");
+  team.innerHTML = "";
+
+  let teamList = [];
+
+  for(const c of compList) {
+    if(c.name == getV("comp")) {
+      teamList = c.teams;
+    }
+  }
+
+  // sort the list for clarity
+  for(let i = 0; i < teamList.length; i++) { 
+    teamList[i] == parseInt(teamList[i]);
+  }
+
+  teamList.sort((a,b) => a-b);
+
+  // add items to dropdown
+  team.appendChild(createOption("","N/A"));
+
+  if(teamList.length == 0) {
+    return;
+  }
+
+  for(const t of teamList) {
+    team.appendChild(createOption(t,t));
+  }
+}
+
+// --------------------------------------------------------------------------------------------------------------------------
+
 // add to storage
 function appendData() {
   const data = {
@@ -117,6 +251,9 @@ function saveForm() {
 // Restore data when moving
 window.onload = function() {
   const data = JSON.parse(localStorage.getItem('flipSave'));
+
+  updateCompList();
+  updateTeamList();
   
   if (data) {
     setV("team",data.team);
